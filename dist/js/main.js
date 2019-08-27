e9c4da1066721827d9c8cfffa2e28d20005a3fdb
Robert Briese <mail@briese-ssc.com>
2019-08-27 22:12:23 +0200

    new updates from andrew
    updates from andrew

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,18 +12,27 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('.single-page-nav').singlePageNav();
+
+  $('#carousel2').owlCarousel({
+    items: 1,
+    dots: true,
+    touchDrag: true
+  });
+
+  checkCookieOnLoad();
 });
+
 
 
 function owlInitialize() {
    if ($(window).width() < 768) {
-      $('.owl-carousel').owlCarousel({
+      $('#carousel1').owlCarousel({
         items: 1,
         dots: true,
         touchDrag: true
       });
    }else{
-      $('.owl-carousel').owlCarousel('destroy');
+      $('#carousel1').owlCarousel('destroy');
    }
 }
 
@@ -33,6 +42,37 @@ $(document).ready(function(e) {
 
 $(window).resize(function() {
    owlInitialize();
+});
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+function checkCookieOnLoad() {
+   if(getCookie("clicklink") === "yes") {
+      $('.legal-terms').removeClass('active');
+   } else {
+      $('.legal-terms').addClass('active');
+   }
+}
+
+$('.ok-button').on('click', function() {
+  setCookie('clicklink', 'yes', 1);
+  checkCookieOnLoad();
 });
 
 //# sourceMappingURL=main.js.map
